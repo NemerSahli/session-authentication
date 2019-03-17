0f986cba1e2a63ee031c3f0d1ea65ccd9bca0b6e
Nemer <nemer.sahli@gmx.de>
2019-03-17 23:40:35 +0100

    App.js added style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Button } from 'reactstrap';
+import { connect } from 'react-redux';
 
 class App extends Component {
   render() {
@@ -9,14 +10,27 @@ class App extends Component {
       <div className="App container center">
         {/* comment */}
         <h1 className="text-center">Session Authentication</h1>
+
         <div className="row justify-content-center">
-        <Button className="btn-danger">login</Button>
-        <Button className="btn-danger">load data from secret area</Button>
-        <Button className="btn-danger">logout</Button>
+          <div className="col-12 text-center">
+            user: {this.props.user.email}
+          </div>{' '}
+          <div className="col-12 text-center">
+            password: {this.props.user.password}
+          </div>
+          <Button className="btn-danger">login</Button>
+          <Button className="btn-danger">load data from secret area</Button>
+          <Button className="btn-danger">logout</Button>
         </div>
       </div>
     );
   }
 }
+const mapStateToProps = state => ({
+  user: state.userReducer.user
+});
 
-export default App;
+export default connect(
+  mapStateToProps,
+  null
+)(App);
